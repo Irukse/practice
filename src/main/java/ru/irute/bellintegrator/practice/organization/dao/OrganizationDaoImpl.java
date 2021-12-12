011b3e24655105e69e93ddccebc8a6d4e78dd580
Irukse <k.irute@yandex.ru>
2021-12-12 15:11:57 +0300

    added method getById save update in OrganizationService and OrganizationController

---
+++ src/main/java/ru/irute/bellintegrator/practice/organization/dao/OrganizationDaoImpl.java
@@ -0,0 +1,48 @@
+package ru.irute.bellintegrator.practice.organization.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ru.irute.bellintegrator.practice.organization.entity.Organization;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Objects;
+
+@Repository
+public class OrganizationDao {
+
+    private final EntityManager em;
+
+    @Autowired
+    public OrganizationDao(EntityManager em){ this.em = em;}
+
+
+
+    public Organization getById(Long id){
+        return em.find(Organization.class, id);
+    }
+
+    public void save(Organization organization) {
+        em.persist(organization);
+    }
+
+    public void update(Organization organization) {
+        Organization existingOrg = em.find(Organization.class, organization.getId());
+        existingOrg.setName(organization.getName());
+        existingOrg.setFullName(organization.getFullName());
+        existingOrg.setInn(organization.getInn());
+        existingOrg.setKpp(organization.getKpp());
+        existingOrg.setAddress(organization.getAddress());
+
+            if (Objects.nonNull(organization.getPhone())) {
+                existingOrg.setPhone(organization.getPhone());
+            }
+
+        existingOrg.setIsActive(true);
+    }
+
+
+    public List<Organization> all() {
+        return em.createQuery("SELECT o FROM Organization o", Organization.class).getResultList();
+    }
+}
